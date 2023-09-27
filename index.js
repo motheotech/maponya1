c5a7f290a31627b2fa4b5383f80925bc6f8521e3
Naledi Motheo <81598687+motheotech@users.noreply.github.com>
2023-09-27 13:34:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    const name = "BIO-CONNECT"
+    let bio = `"
+            Bio Konnect is platform that connects locals\community to economical opportunities of buying and selling used cooking oil.We act as a channel for oil bankers to trade oil to bio manufactures."`
+    let address = ` D1809 Chris Hani Rd, \n
+                    Klipspruit, \n
+                    Soweto, \n
+                    1862`
+                   
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("bio").innerHTML = bio;
+    document.getElementById("address").innerHTML = address;
+}
